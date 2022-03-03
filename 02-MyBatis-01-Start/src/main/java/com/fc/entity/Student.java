a7dae64a8b3d4f75bde6e3a3c7ceb00f041d81c3
yanyuqi <3335939034@qq.com>
2022-03-03 14:57:33 +0800

    Batis

---
+++ 02-MyBatis-01-Start/src/main/java/com/fc/entity/Student.java
@@ -0,0 +1,12 @@
+package com.fc.entity;
+
+import java.util.Date;
+
+public class Student {
+    private  Integer id;
+    private  String name;
+    private  Byte age;
+    private  String gender;
+    private Date birthday;
+    private  String  info;
+}
